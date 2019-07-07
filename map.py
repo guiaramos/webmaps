b69c74b8918b3f11cd4b16b91c2508d088f5e02c
Guilherme <gui_aramos@outlook.com>
2019-07-07 23:46:46 +0900

    Updated: Added Source, Marks loop and Code Comments

--- map.py
+++ map.py
@@ -1,15 +1,30 @@
-import folium
+# Libraries #
+import folium, pandas
 
-startLocation = [38.58, -99.09]
+# Map Variables #
+coordinations = [[38.58, -99.09], [39.2, -97.1]]
 startZoom = 6
 startTile = "Stamen Terrain"
 
-map = folium.Map(location=startLocation, zoom_start=startZoom, tile=startTile)
+# Source Variables #
+data = pandas.read_csv("src/Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
 
+# Start Program #
+
+# Creating Map
+map = folium.Map(location=coordinations[0], zoom_start=startZoom, tile=startTile)
+
+# Defining Group for the map
 fg = folium.FeatureGroup(name="My Map")
 
-fg.add_child(folium.Marker(location=startLocation, popup="This is a Marker", icon=folium.Icon(color='green')))
+# Creating the Marks
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln], popup="This is a Marker", icon=folium.Icon(color='green')))
 
+# Adding the Marks
 map.add_child(fg)
 
+# Creating HTML
 map.save("Map1.html")
