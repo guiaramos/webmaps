3d1e6dadeb8efa0ca6d6fcc76687d0322c3bcd4c
Guilherme <gui_aramos@outlook.com>
2019-07-08 00:16:25 +0900

    Updated: Added HTML pointer with names and link to google

--- map.py
+++ map.py
@@ -11,6 +11,15 @@ data = pandas.read_csv("src/Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
+name = list(data["NAME"])
+
+# HTML format #
+html = """
+<h4>Volcano information:</h4
+<h5>Volcano name:</h5>
+<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
+Height: %s m
+"""
 
 # Start Program #
 
@@ -21,11 +30,12 @@ map = folium.Map(location=coordinations[0], zoom_start=startZoom, tile=startTile
 fg = folium.FeatureGroup(name="My Map")
 
 # Creating the Marks
-for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=el, icon=folium.Icon(color='green')))
+for lt, ln, el, name in zip(lat, lon, elev, name):
+    iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color='green')))
 
 # Adding the Marks
 map.add_child(fg)
 
 # Creating HTML
-map.save("Map1.html")
+map.save("Map_html_popup_simple.html")
